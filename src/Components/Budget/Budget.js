bea15fded9d3b4882a06e5431cd4aeb35afeaacb
старейшина <blakedyblake@gmail.com>
2021-09-29 12:09:59 -0600

    Finished the Project
    Bug: I have to refresh it everytime it sumbits a delete or add for it to show up

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -8,23 +8,36 @@ import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
 
+import {connect} from 'react-redux'
+import { requestUserData } from '../../redux/useReducer';
+import {requestBudgetData,addPurchase,removePurchase} from '../../redux/budgetReducer'
+
 
 class Budget extends Component {
+  componentDidMount(){
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+    // this.props.addPurchase()
+    // this.props.removePurchase()
 
+  }
   render() {
+    const {loading, purchases, budgetLimit} = this.props.budget
+    const {firstName, lastName} = this.props.user
+    const {addPurchase, removePurchase} = this.props
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={addPurchase}/>
+              <DisplayPurchases purchases={purchases} removePurchase={removePurchase}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit}/>
+              <Chart2 purchases={purchases}/>
             </div>
           </div>
         </div>
@@ -32,5 +45,11 @@ class Budget extends Component {
     )
   }
 }
+function mapStateToProps(state){
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
 
-export default Budget;
+export default connect(mapStateToProps,{requestUserData, requestBudgetData, addPurchase,removePurchase})(Budget);
